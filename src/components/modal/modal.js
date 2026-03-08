06cc7094c99b87721503b37380d16bb7ae355661
mizza-cz <mizza.cz666@gmail.com>
2026-03-08 17:53:42 +0100

    fix js

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,19 +3,12 @@ document.addEventListener("click", function (e) {
   if (a) e.preventDefault();
 });
 
-const modal = document.getElementById("articleModal");
-
-if (modal) {
+document.querySelectorAll(".modal").forEach((modal) => {
   modal.addEventListener("hidden.bs.modal", function () {
-    modal.querySelectorAll("video").forEach((video) => {
-      video.pause();
-      video.currentTime = 0;
-    });
-
     modal.querySelectorAll("iframe").forEach((iframe) => {
-      const src = iframe.getAttribute("src");
-      iframe.setAttribute("src", "");
-      iframe.setAttribute("src", src);
+      const src = iframe.src;
+      iframe.src = "";
+      iframe.src = src;
     });
   });
-}
+});
