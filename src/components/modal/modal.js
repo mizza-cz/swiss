54f0489fded205ca43deaf4a1d4c1a5e02713658
mizza-cz <mizza.cz666@gmail.com>
2026-03-08 11:29:40 +0100

    fixes

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,3 +2,15 @@ document.addEventListener("click", function (e) {
   const a = e.target.closest('a[data-bs-toggle="modal"]');
   if (a) e.preventDefault();
 });
+
+const modal = document.getElementById("articleModal");
+
+if (modal) {
+  modal.addEventListener("hidden.bs.modal", function () {
+    const iframes = modal.querySelectorAll("iframe");
+
+    iframes.forEach((iframe) => {
+      iframe.src = iframe.src;
+    });
+  });
+}
